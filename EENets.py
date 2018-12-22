d38d081061dba443a513aca15b68a9bd996bb2df
Edanur Demir <eksuas@gmail.com>
2018-12-22 21:34:19 +0300

    Add files via upload

--- EENets.py
+++ EENets.py
@@ -3,67 +3,67 @@ import torch.nn as nn
 import torchvision.models as models
 
 class EENet(nn.Module):
-    def __init__(self):
-        super(EENet, self).__init__()
+	def __init__(self):
+		super(EENet, self).__init__()
         self.initblock = nn.Sequential(
-        	nn.Conv2d(1, 4, kernel_size=3, stride=1, padding=1),
-        	nn.BatchNorm2d(4),
+			nn.Conv2d(1, 4, kernel_size=3, stride=1, padding=1),
+			nn.BatchNorm2d(4),
 			nn.ReLU(inplace=True),
-        )
-        self.basicblock1 = nn.Sequential(
-        	nn.Conv2d(4, 4, kernel_size=3, stride=1, padding=1),
-        	nn.BatchNorm2d(4),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(4, 4, kernel_size=3, stride=1, padding=1),
-        )
-        self.basicblock2 = nn.Sequential(
-            nn.BatchNorm2d(4),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(4, 8, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm2d(8),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(8, 8, kernel_size=3, stride=1, padding=1),
-        )
-        self.basicblock3 = nn.Sequential(
-            nn.BatchNorm2d(8),
-            nn.ReLU(inplace=True),
-    		nn.Conv2d(8, 16, kernel_size=3, stride=2, padding=1),
-    		nn.BatchNorm2d(16),
-            nn.ReLU(inplace=True),
-    		nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1),
-        )
-        self.finalblock = nn.Sequential(
-            nn.BatchNorm2d(16),
-            nn.ReLU(inplace=True),
-    		nn.AdaptiveAvgPool2d(1),
-        )
-        self.conv2d_6 = nn.Conv2d(4, 8, kernel_size=1, stride=2, padding=0)
-        self.conv2d_9 = nn.Conv2d(8, 16, kernel_size=1, stride=2, padding=0)
-        self.pool = nn.AdaptiveAvgPool2d(1)
+		)
+		self.basicblock1 = nn.Sequential(
+			nn.Conv2d(4, 4, kernel_size=3, stride=1, padding=1),
+			nn.BatchNorm2d(4),
+			nn.ReLU(inplace=True),
+			nn.Conv2d(4, 4, kernel_size=3, stride=1, padding=1),
+		)
+		self.basicblock2 = nn.Sequential(
+			nn.BatchNorm2d(4),
+			nn.ReLU(inplace=True),
+			nn.Conv2d(4, 8, kernel_size=3, stride=2, padding=1),
+			nn.BatchNorm2d(8),
+			nn.ReLU(inplace=True),
+			nn.Conv2d(8, 8, kernel_size=3, stride=1, padding=1),
+		)
+		self.basicblock3 = nn.Sequential(
+			nn.BatchNorm2d(8),
+			nn.ReLU(inplace=True),
+			nn.Conv2d(8, 16, kernel_size=3, stride=2, padding=1),
+			nn.BatchNorm2d(16),
+			nn.ReLU(inplace=True),
+			nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1),
+		)
+		self.finalblock = nn.Sequential(
+			nn.BatchNorm2d(16),
+			nn.ReLU(inplace=True),
+			nn.AdaptiveAvgPool2d(1),
+		)
+		self.conv2d_6 = nn.Conv2d(4, 8, kernel_size=1, stride=2, padding=0)
+		self.conv2d_9 = nn.Conv2d(8, 16, kernel_size=1, stride=2, padding=0)
+		self.pool = nn.AdaptiveAvgPool2d(1)
 
-        self.exit0_classifier = nn.Sequential(
-            nn.Linear(4, 10),
-            nn.Softmax(dim=1),
-        )
-        self.exit1_classifier = nn.Sequential(
-            nn.Linear(8, 10),
-            nn.Softmax(dim=1),
-        )
-        self.exit0_confidence = nn.Sequential(
-            nn.Linear(4, 1),
-            nn.Sigmoid(),
-        )
-        self.exit1_confidence = nn.Sequential(
-            nn.Linear(8, 1),
-            nn.Sigmoid(),
-        )
-        self.classifier = nn.Sequential(
-            nn.Linear(16, 10),
-            nn.Softmax(dim=1),
-        )
+		self.exit0_classifier = nn.Sequential(
+			nn.Linear(4, 10),
+			nn.Softmax(dim=1),
+		)
+		self.exit1_classifier = nn.Sequential(
+			nn.Linear(8, 10),
+			nn.Softmax(dim=1),
+		)
+		self.exit0_confidence = nn.Sequential(
+			nn.Linear(4, 1),
+			nn.Sigmoid(),
+		)
+		self.exit1_confidence = nn.Sequential(
+			nn.Linear(8, 1),
+			nn.Sigmoid(),
+		)
+		self.classifier = nn.Sequential(
+			nn.Linear(16, 10),
+			nn.Softmax(dim=1),
+		)
 
-    def forward(self, x):
-        x = self.initblock(x)
+	def forward(self, x):
+  		x = self.initblock(x)
         residual = self.basicblock1(x)
         x = residual + x
 
