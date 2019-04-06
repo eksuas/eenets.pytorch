81b7d903901b1a920ef4e2cda895ae8eb0286f8d
Edanur Demir <eksuas@gmail.com>
2019-04-06 21:13:38 +0300

    pylint check

--- custom_eenet.py
+++ custom_eenet.py
@@ -1,103 +1,113 @@
-import torch
-import torch.nn as nn
+"""
+Custom EENet model
+"""
+from torch import nn
 
 __all__ = ['CustomEENet', 'eenet8']
 
 class CustomEENet(nn.Module):
-	def __init__(self, filter):
-		super(CustomEENet, self).__init__()
-		self.filter = filter
-		self.initblock = nn.Sequential(
-			nn.Conv2d(1, filter, kernel_size=3, stride=1, padding=1),
-			nn.BatchNorm2d(filter),
-			nn.ReLU(inplace=True),
-		)
-		self.basicblock1 = nn.Sequential(
-			nn.Conv2d(filter, filter, kernel_size=3, stride=1, padding=1),
-			nn.BatchNorm2d(filter),
-			nn.ReLU(inplace=True),
-			nn.Conv2d(filter, filter, kernel_size=3, stride=1, padding=1),
-		)
-		self.basicblock2 = nn.Sequential(
-			nn.BatchNorm2d(filter),
-			nn.ReLU(inplace=True),
-			nn.Conv2d(filter, filter*2, kernel_size=3, stride=2, padding=1),
-			nn.BatchNorm2d(filter*2),
-			nn.ReLU(inplace=True),
-			nn.Conv2d(filter*2, filter*2, kernel_size=3, stride=1, padding=1),
-		)
-		self.basicblock3 = nn.Sequential(
-			nn.BatchNorm2d(filter*2),
-			nn.ReLU(inplace=True),
-			nn.Conv2d(filter*2, filter*4, kernel_size=3, stride=2, padding=1),
-			nn.BatchNorm2d(filter*4),
-			nn.ReLU(inplace=True),
-			nn.Conv2d(filter*4, filter*4, kernel_size=3, stride=1, padding=1),
-		)
-		self.finalblock = nn.Sequential(
-			nn.BatchNorm2d(filter*4),
-			nn.ReLU(inplace=True),
-			nn.AdaptiveAvgPool2d(1),
-		)
-		self.conv2d_6 = nn.Conv2d(filter, filter*2, kernel_size=1, stride=2, padding=0)
-		self.conv2d_9 = nn.Conv2d(filter*2, filter*4, kernel_size=1, stride=2, padding=0)
-		self.pool = nn.AdaptiveAvgPool2d(1)
+    """Custom EENet-8 model.
 
-		self.exit0_classifier = nn.Sequential(
-			nn.Linear(filter, 10),
-			nn.Softmax(dim=1),
-		)
-		self.exit1_classifier = nn.Sequential(
-			nn.Linear(filter*2, 10),
-			nn.Softmax(dim=1),
-		)
-		self.exit0_confidence = nn.Sequential(
-			nn.Linear(filter, 1),
-			nn.Sigmoid(),
-		)
-		self.exit1_confidence = nn.Sequential(
-			nn.Linear(filter*2, 1),
-			nn.Sigmoid(),
-		)
-		self.classifier = nn.Sequential(
-			nn.Linear(filter*4, 10),
-			nn.Softmax(dim=1),
-		)
-		self.cost = [0.60, 0.97]
+    This model has identity connections such as ResNets.
+    However, EENet-8 is a very small CNN having 2-8 filters in its layers.
+    """
+    def __init__(self, starting_filter):
+        super(CustomEENet, self).__init__()
+        self.filter = starting_filter
+        self.initblock = nn.Sequential(
+            nn.Conv2d(1, self.filter, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(self.filter),
+            nn.ReLU(inplace=True),
+        )
+        self.basicblock1 = nn.Sequential(
+            nn.Conv2d(self.filter, self.filter, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(self.filter),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(self.filter, self.filter, kernel_size=3, stride=1, padding=1),
+        )
+        self.basicblock2 = nn.Sequential(
+            nn.BatchNorm2d(self.filter),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(self.filter, self.filter*2, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(self.filter*2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(self.filter*2, self.filter*2, kernel_size=3, stride=1, padding=1),
+        )
+        self.basicblock3 = nn.Sequential(
+            nn.BatchNorm2d(self.filter*2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(self.filter*2, self.filter*4, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(self.filter*4),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(self.filter*4, self.filter*4, kernel_size=3, stride=1, padding=1),
+        )
+        self.finalblock = nn.Sequential(
+            nn.BatchNorm2d(self.filter*4),
+            nn.ReLU(inplace=True),
+            nn.AdaptiveAvgPool2d(1),
+        )
+        self.conv2d_6 = nn.Conv2d(self.filter, self.filter*2, kernel_size=1, stride=2, padding=0)
+        self.conv2d_9 = nn.Conv2d(self.filter*2, self.filter*4, kernel_size=1, stride=2, padding=0)
+        self.pool = nn.AdaptiveAvgPool2d(1)
 
-	def forward(self, x):
-		x = self.initblock(x)
-		residual = self.basicblock1(x)
-		x = residual + x
+        self.exit0_classifier = nn.Sequential(
+            nn.Linear(self.filter, 10),
+            nn.Softmax(dim=1),
+        )
+        self.exit1_classifier = nn.Sequential(
+            nn.Linear(self.filter*2, 10),
+            nn.Softmax(dim=1),
+        )
+        self.exit0_confidence = nn.Sequential(
+            nn.Linear(self.filter, 1),
+            nn.Sigmoid(),
+        )
+        self.exit1_confidence = nn.Sequential(
+            nn.Linear(self.filter*2, 1),
+            nn.Sigmoid(),
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(self.filter*4, 10),
+            nn.Softmax(dim=1),
+        )
+        self.cost = [0.60, 0.97]
 
-		e_x = self.pool(x).view(-1, self.filter)
-		y0 = self.exit0_classifier(e_x)
-		h0 = self.exit0_confidence(e_x)
-		if (not self.training and h0.item() > 0.5):
-			return y0, 0, self.cost[0]
+    def forward(self, x):
+        x = self.initblock(x)
+        residual = self.basicblock1(x)
+        x = residual + x
 
-		residual = self.basicblock2(x)
-		x = self.conv2d_6(x)
-		x = residual + x
+        e_x = self.pool(x).view(-1, self.filter)
+        pred_0 = self.exit0_classifier(e_x)
+        conf_0 = self.exit0_confidence(e_x)
+        if (not self.training and conf_0.item() > 0.5):
+            return pred_0, 0, self.cost[0]
 
-		e_x = self.pool(x).view(-1, self.filter*2)
-		y1 = self.exit1_classifier(e_x)
-		h1 = self.exit1_confidence(e_x)
-		if (not self.training and h1.item() > 0.5):
-			return y1, 1, self.cost[1]
+        residual = self.basicblock2(x)
+        x = self.conv2d_6(x)
+        x = residual + x
 
-		residual = self.basicblock3(x)
-		x = self.conv2d_9(x)
-		x = residual + x
-		x = self.finalblock(x)
-		x = x.view(-1, self.filter*4)
-		y2 = self.classifier(x)
-		if (not self.training):
-			return y2, 2, 1.0
+        e_x = self.pool(x).view(-1, self.filter*2)
+        pred_1 = self.exit1_classifier(e_x)
+        conf_1 = self.exit1_confidence(e_x)
+        if (not self.training and conf_1.item() > 0.5):
+            return pred_1, 1, self.cost[1]
 
-		return (y0, y1, y2), (h0, h1), self.cost
+        residual = self.basicblock3(x)
+        x = self.conv2d_9(x)
+        x = residual + x
+        x = self.finalblock(x)
+        x = x.view(-1, self.filter*4)
+        pred_2 = self.classifier(x)
+        if not self.training:
+            return pred_2, 2, 1.0
+
+        return (pred_0, pred_1, pred_2), (conf_0, conf_1), self.cost
 
 
-def eenet8(filters=2, **kwargs):
-    model = CustomEENet(filters)
-    return model
+def eenet8(filters=2):
+    """EENet-8 model.
+
+    This creates an instance of Custom EENet-8 with given starting filter number.
+    """
+    return CustomEENet(filters)
--- utils.py
+++ utils.py
@@ -1,123 +1,124 @@
-import torch
+"""
+utilities are defined in this code.
+"""
 import os
+import torch
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 from torchvision import datasets, transforms
-from custom_eenet import *
-from eenet import *
+from custom_eenet import CustomEENet
+from eenet import EENet
 
-class AverageMeter(object):
+class AverageMeter():
     """Computes and stores the average and current value"""
     def __init__(self):
         self.reset()
 
     def reset(self):
+        """reset the meter"""
         self.val = 0.0
         self.avg = 0.0
         self.sum = 0.0
         self.count = 0.0
 
-    def update(self, val, n=1):
+    def update(self, val, repetition=1):
+        """update the meter"""
         self.val = val
-        self.sum += val * n
-        self.count += n
+        self.sum += val * repetition
+        self.count += repetition
         self.avg = self.sum / self.count
 
 
-def load_dataset(args, use_cuda):
-    kwargs = {'num_workers': 2, 'pin_memory': True} if use_cuda else {}
+def load_dataset(args):
+    """dataset loader.
 
-    if (args.dataset == 'mnist'):
+    This loads the dataset.
+    """
+    kwargs = {'num_workers': 2, 'pin_memory': True} if args.device == 'cuda' else {}
+
+    if args.dataset == 'mnist':
         root = '../data/mnist'
         transform = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.1307,), (0.3081,))
         ])
         trainset = datasets.MNIST(root=root, train=True, download=True, transform=transform)
-        testset  = datasets.MNIST(root=root, train=False, download=True, transform=transform)
+        testset = datasets.MNIST(root=root, train=False, download=True, transform=transform)
 
-    elif (args.dataset == 'cifar10'):
+    elif args.dataset == 'cifar10':
         root = '../data/cifar10'
-        trainset = datasets.CIFAR10(root=root, train=True, download=True,
-            transform=transforms.Compose([
+        trainset = datasets.CIFAR10(root=root, train=True, download=True,\
+            transform=transforms.Compose([\
                     transforms.RandomCrop(32, padding=4),
                     transforms.RandomHorizontalFlip(),
                     transforms.ToTensor(),
-                    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
-            ]))
+                    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))]))
 
-
-        testset = datasets.CIFAR10(root=root, train=False, download=True,
-            transform=transforms.Compose([
+        testset = datasets.CIFAR10(root=root, train=False, download=True,\
+            transform=transforms.Compose([\
                     transforms.ToTensor(),
-                    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
-            ]))
+                    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))]))
 
-    elif (args.dataset == 'svhn'):
-        def target_transform(target):
-            return target[0]-1
+    elif args.dataset == 'svhn':
+        #def target_transform(target):
+        #    return target[0]-1
 
         root = '../data/svhn'
-        transform = transforms.Compose([
+        transform = transforms.Compose([\
             transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-        ])
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
         trainset = datasets.SVHN(root=root, split='train', download=True, transform=transform)
                     #,target_transform=target_transform)
-        testset  = datasets.SVHN(root=root, split='test', download=True, transform=transform)
+        testset = datasets.SVHN(root=root, split='test', download=True, transform=transform)
                     #,target_transform=target_transform)
 
-    elif (args.dataset == 'imagenet'):
+    elif args.dataset == 'imagenet':
         root = '../data/imagenet'
-        trainset = datasets.ImageFolder(root=root+'/train', transform=transforms.Compose([
+        trainset = datasets.ImageFolder(root=root+'/train', transform=transforms.Compose([\
             transforms.RandomResizedCrop(224),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        ]))
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]))
 
-        testset  = datasets.ImageFolder(root=root+'/val', transform=transforms.Compose([
+        testset = datasets.ImageFolder(root=root+'/val', transform=transforms.Compose([\
             transforms.Resize(256),
             transforms.CenterCrop(224),
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        ]))
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]))
 
-    elif (args.dataset == 'tiny-imagenet'):
+    elif args.dataset == 'tiny-imagenet':
         create_val_img_folder()
         root = '../data/tiny-imagenet'
-        trainset = datasets.ImageFolder(root=root+'/train', transform=transforms.Compose([
+        trainset = datasets.ImageFolder(root=root+'/train', transform=transforms.Compose([\
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
-        ]))
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])]))
 
-        testset  = datasets.ImageFolder(root=root+'/val/images', transform=transforms.Compose([
+        testset = datasets.ImageFolder(root=root+'/val/images', transform=transforms.Compose([\
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
-        ]))
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])]))
 
-
-
-    train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size,
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size,\
                     shuffle=True, **kwargs)
-    test_loader  = torch.utils.data.DataLoader(testset, batch_size=args.test_batch,
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=args.test_batch,\
                     shuffle=False, **kwargs)
     return train_loader, test_loader
 
 
 def create_val_img_folder():
-    '''
-    This method is responsible for separating validation images into separate sub folders
-    '''
+    """
+    This method is responsible for separating validation images into separate sub folders.
+    """
     val_dir = '../data/tiny-imagenet/val'
     img_dir = os.path.join(val_dir, 'images')
 
-    fp = open(os.path.join(val_dir, 'val_annotations.txt'), 'r')
-    data = fp.readlines()
+    file = open(os.path.join(val_dir, 'val_annotations.txt'), 'r')
+    data = file.readlines()
     val_img_dict = {}
     for line in data:
         words = line.split('\t')
         val_img_dict[words[0]] = words[1]
-    fp.close()
+    file.close()
 
     # Create folder if not present and move images into proper folders
     for img, folder in val_img_dict.items():
@@ -128,8 +129,11 @@ def create_val_img_folder():
             os.rename(os.path.join(img_dir, img), os.path.join(newpath, img))
 
 
-def plotCharts (history, args):
-    fig, axs = plt.subplots(1,1)
+def plot_charts(history, args):
+    """
+    This method plots the history charts.
+    """
+    fig, axs = plt.subplots(1, 1)
     plt.title('The EENet-8 model trained with the '+args.filename+' loss')
     legend = []
     for key, value in history.items():
@@ -144,32 +148,39 @@ def plotCharts (history, args):
     plt.clf()
 
 
-def display_examples(args, model, device, dataset):
+def display_examples(args, model, dataset):
+    """
+    This method shows the correctly predicted sample of images from dataset.
+    Produced table shows the early exit block which classifies that samples.
+    """
     images = [[[] for j in range(10)] for i in range(args.num_ee+1)]
     model.eval()
     with torch.no_grad():
         for idx, (data, target) in enumerate(dataset):
             data = data.view(-1, *args.input_shape)
-            data, target = data.to(device), target.to(device).item()
-            output, exit, _ = model(data)
+            data, target = data.to(args.device), target.to(args.device).item()
+            output, idx, _ = model(data)
             # get the index of the max log-probability
             pred = output.max(1, keepdim=True)[1].item()
             if pred == target:
-                if len(images[exit][target]) < 10:
-                    images[exit][target].append(idx)
+                if len(images[idx][target]) < 10:
+                    images[idx][target].append(idx)
 
-        for exit in range(args.num_ee+1):
+        for idx in range(args.num_ee+1):
             fig, axarr = plt.subplots(10, 10)
             for class_id in range(10):
                 for example in range(10):
                     axarr[class_id, example].axis('off')
-                for example in range(len(images[exit][class_id])):
+                for example in range(len(images[idx][class_id])):
                     axarr[class_id, example].imshow(
-                        dataset[images[exit][class_id][example]][0].view(args.input_shape[1:]))
-            fig.savefig("Results/exitblock"+str(exit)+".png")
+                        dataset[images[idx][class_id][example]][0].view(args.input_shape[1:]))
+            fig.savefig("Results/exitblock"+str(idx)+".png")
 
 
 def save_model(args, model):
+    """
+    This method saves the trained model in pt file.
+    """
     directory = '../models/'+args.model
     if not os.path.exists(directory):
         os.makedirs(directory)
@@ -183,14 +194,16 @@ def save_model(args, model):
 
 
 def adjust_learning_rate(model, optimizer, epoch):
-    lr = 0.1
+    """
+    This method adjusts the learning rate of training.
+    """
+    learning_rate = 0.1
     if epoch > 150:
-        lr = 0.01
+        learning_rate = 0.01
     if epoch > 250:
-        lr = 0.001
-
-    if isinstance(model, EENet) or isinstance(model, CustomEENet):
-        lr *= 0.01
+        learning_rate = 0.001
+    if isinstance(model, (EENet, CustomEENet)):
+        learning_rate *= 0.01
 
     for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
+        param_group['lr'] = learning_rate
